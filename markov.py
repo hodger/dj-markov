e63ad7f6fd5770263f7f66ee9900eed517727ffc
Ryan Hodge <rhodge@email.sc.edu>
2017-01-11 12:16:22 -0500

    added consume, basic tests

--- markov.py
+++ markov.py
@@ -5,20 +5,24 @@ import numpy as np
 from random import randint
 import json
         
-class Markov(object):
+class Markov(object):   
+    count_index = 0
+    freq_index = 1
     def __init__(self, obj_list=None, load=None):
         self.freq_table = None
-        
-        self.dict_index = 0
-        self.inner_count_index = 0
-        self.inner_freq_index = 1
-        self.total_index = 1
-        
+        self.totals = None
         self.root = None
         self.chain = None
         self.reset()
         if load:
             self.freq_table = json.load(open(load, 'r'))
+            for k in self.freq_table:
+                self.freq_table[k] = defaultdict(lambda: [0, 0.0], self.freq_table[k])
+                total = 0
+                for ik in self.freq_table[k]:
+                    total += self.freq_table[k][ik][Markov.count_index]
+                self.totals[k] = total
+            self.freq_table = defaultdict(lambda: defaultdict(lambda: [0, 0.0]), self.freq_table)
         if obj_list:
             self.add(obj_list)
     
@@ -35,19 +39,20 @@ class Markov(object):
     #Helper function for add_text.
     #Adds a word to the frequency table WITHOUT recalculating frequency value.
     def _add_pair(self, orig, after):
-        self.freq_table[orig][self.dict_index][after][self.inner_count_index] += 1
-        self.freq_table[orig][self.total_index] += 1
+        self.freq_table[orig][after][Markov.count_index] += 1
+        self.totals[orig] += 1
     
     #Calculates frequencies of potential next words for every original word.
     def _calc_frequency(self, changed_keys=None):
         if changed_keys is None:
             changed_keys = list(self.freq_table.keys())
         for k in changed_keys:
-            for ik in list(self.freq_table[k][self.dict_index].keys()):
-                self.freq_table[k][self.dict_index][ik][self.inner_freq_index] = self.freq_table[k][self.dict_index][ik][self.inner_count_index] / self.freq_table[k][self.total_index]
+            for ik in self.freq_table[k]:
+                self.freq_table[k][ik][Markov.freq_index] = self.freq_table[k][ik][Markov.count_index] / self.totals[k]
     
     def reset(self):
-        self.freq_table = defaultdict(lambda: [defaultdict(lambda: [0, 0.0]), 0])
+        self.freq_table = defaultdict(lambda: defaultdict(lambda: [0, 0.0]))
+        self.totals = defaultdict(int)
         self.root = None
         self.chain = None
     
@@ -73,18 +78,33 @@ class Markov(object):
                 result.append(next(self.chain))
                 chain_index += 1
             except StopIteration:
-                if restart_on_error:
+                if restart_on_error: 
                     if not min_dist is None:
                         if chain_index >= (min_dist - 1):
                             break                    
-                    self.root == self.random()
+                    self.root = self.random()
                     self.chain = self.gen_chain()
                     continue
                 else:
                     break
         return result
     
-    def best(dist, root=None):
+    def best(self, orig):
+        next_words = self.retrieve(orig)
+        if not next_words:
+            raise ValueError("No next objects available")
+        best_freq = 0.0
+        best_words = []
+        for word, freq in next_words:
+            if freq > best_freq:
+                best_words = [word]
+                best_freq = freq
+            elif freq == best_freq:
+                best_words.append(word)
+        return best_words[randint(0, len(best_words) - 1)]
+    
+    #TODO: use best to make this work
+    def best_chain(self, dist, root=None):
         if not self.freq_table:
             raise ValueError("No text available")
         if root is None:
@@ -122,14 +142,13 @@ class Markov(object):
                 yield new_obj
         
     def next_objs(self, orig):
-        next_objs = list(self.freq_table[orig][self.dict_index].keys())
+        next_objs = list(self.freq_table[orig].keys())
         return next_objs
     
     #Return a list of tuples in the format of [("next_word", frequency)]
     def retrieve(self, orig):
-        freq_list = [(i, self.freq_table[orig][self.dict_index][i]
-                                              [self.inner_freq_index]) 
-                         for i in self.freq_table[orig][self.dict_index].keys()]
+        freq_list = [(i, self.freq_table[orig][i][Markov.freq_index]) 
+                         for i in self.freq_table[orig].keys()]
         return freq_list
     
     #Print entire frequency table, prettily.
@@ -143,14 +162,34 @@ class Markov(object):
         print(orig, ":", freq_list)
     
     def dumps(self):
-        for i in self.freq_table:
-            print(i, self.freq_table[i])
+        for key in self.freq_table:
+            print(key, dict(self.freq_table[key]))
             
-    def consume(target):
+    def dump_totals(self):
+        for key in self.totals:
+            print(key, self.totals[key])
+            
+    def consume(self, target):
         #Iterate over rows of frequency table, borrowing rows with new keys,
         #adjusting and recalculating frequencies of rows with a familiar key. 
-        pass
-        
+        changed_keys = []
+        for key in target.freq_table:
+            vals = target.freq_table[key]
+            if key in self.freq_table:
+                additional = 0
+                for i in vals:
+                    additional += vals[i][Markov.count_index]
+                    if i in self.freq_table[key]:
+                        self.freq_table[key][i][Markov.count_index] += vals[i][Markov.count_index]
+                    else:
+                        self.freq_table[key][i] = vals[i]    
+                changed_keys.append(key)
+                self.totals[key] += additional        
+            else:
+                self.freq_table[key] = vals
+                self.totals[key] = target.totals[key]
+        self._calc_frequency(changed_keys=changed_keys)
+          
     def save(self, name):
         if self.freq_table:
             f = open(name, 'w')
---
+++ markov_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from markov import Markov
+
+chain = Markov(obj_list=['hello', 'world', 'sup', 'hello', 'there', 'sup', 'bro'])
+chain.dumps()
+chain.dump_totals()
+print(chain.traverse(5))
+chain.save('test_chain.txt')
+
+print("")
+print("")
+
+#copy chain from file
+new_chain = Markov(load='test_chain.txt', obj_list=['hello', 'nuts'])
+new_chain.dumps()
+new_chain.dump_totals()
+print(new_chain.traverse(5))
+new_chain.dumps()
+
+print("")
+print("")
+
+chain.consume(new_chain)
+chain.dumps()
+chain.dump_totals()
